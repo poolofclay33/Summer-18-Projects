b6fd668962eed279db0acbe870b4691657a8413c
Brady <poolofclay33@gmail.com>
2018-05-18 12:32:11 -0500

    Add Project

---
+++ Summer '18/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour {
+
+    public GameObject loadingScreen;
+    public Slider slider;
+
+    public void LoadLevel (int sceneIndex) 
+    {
+        StartCoroutine(Load(sceneIndex));
+    }
+
+    IEnumerator Load (int sceneIndex) 
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex); //Loads the Scene asynchronously in the background.
+
+        loadingScreen.SetActive(true);
+
+        while (operation.isDone == false) 
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            Debug.Log(operation.progress);
+            Debug.Log("**************");
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+}
---
+++ Summer '18/Assets/Scripts/PlayerMovement2D.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour {
+
+	private float x;
+	private float y;
+	public float moveSpeed;
+
+	void FixedUpdate() {
+		x = Input.GetAxis ("Horizontal");
+		y = Input.GetAxis ("Vertical");
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (x * moveSpeed, y * moveSpeed);
+	}
+}
